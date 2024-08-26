6c31ef005627463b88319341a56446400600bb08
AWS CCP <aws.ccp21@gmail.com>
2024-08-26 23:48:23 +0000

    ModelMonitor : Added designPipeline.ipynb

---
+++ pipelines/deployment/preprocessRawData.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def preprocess_raw_data(raw_data_df):
+    # this is a dummy feature engineering function to pre-process raw data
+    # and make it ready for model consumption. 
+    # in real world use case, it will contain more sophisticated code
+    # that perform feature engineering and derive new features.
+    # for demo purpose, we are simply returning it without processing anything.
+    
+    feature_engineered_df = raw_data_df
+    
+    return feature_engineered_df
